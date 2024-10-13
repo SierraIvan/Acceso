494134bda9aa22cef8dd4ef887411dc88711306d
Iván Sierra <ivan.sierra.perez@colegiolitterator.com>
2024-10-13 20:11:05 +0200

    arbol

--- arboles/src/arbol1/main.java
+++ arboles/src/arbol1/main.java
@@ -3,7 +3,21 @@ package arbol1;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ejercicio1 arbol = new ejercicio1();
+		
+		arbol.addElem(4);
+		arbol.addElem(1);
+		arbol.addElem(2);
+		arbol.addElem(7);
+		arbol.addElem(9);
+
+		
+		System.out.println(arbol.shearchElem(9));
+		System.out.println(arbol.estricto());
+		System.out.println(arbol.altura());
+		System.out.println(arbol.maximo());
+		System.out.println(arbol.cuenta());
 
 	}
 
